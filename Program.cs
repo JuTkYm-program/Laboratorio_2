9a0b39b303d31189c11eaf7dd1cd8b937aebd084
JuTkYm-program <justynmonago@gmail.com>
2026-09-20 16:43:33 -0500

    Menu Principal y Requerimiento 2: Validar código no vacío y longitud mínima

--- Program.cs
+++ Program.cs
@@ -1,9 +1,34 @@
-﻿static void Main()
-    {
-       
+﻿class Sisteam
+{
+static void Main()
+ {
+     
+    System.Console.WriteLine("=== SISTEMA DE SOPORTE ACADÉMICO ===");
 
-            // Requerimiento 1: Datos básicos de la solicitud
-            string codigo = "", nombre = "", tipoConsulta = "", descripcion = "";
+     
+     
+      
+      System.Console.WriteLine("\n--- Iniciando Registro de Solicitud  ---");
+        string codigo = "", nombre = "", tipoConsulta = "", descripcion = "";
 
-         
-    }
+      
+      
+         System.Console.WriteLine("Ingrese Código de Estudiante (mínimo 6 caracteres): ");
+         codigo = Console.ReadLine();
+      
+
+         System.Console.WriteLine("Ingrese Nombre del Estudiante: ");
+         nombre = Console.ReadLine();
+
+         System.Console.WriteLine("Ingrese Tipo de Consulta: ");
+         tipoConsulta = Console.ReadLine().ToLower();
+
+         System.Console.WriteLine("Ingrese Descripción breve: ");
+         descripcion = Console.ReadLine();
+      
+    
+     
+ }
+
+}
+ 
