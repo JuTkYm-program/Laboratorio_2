b2f753cb9e186ded12c1aab485846607787bba01
JuTkYm-program <justynmonago@gmail.com>
2026-09-20 20:28:31 -0500

    Req 12: Documentación de los requerimientos atendidos por cada función

--- Program.cs
+++ Program.cs
@@ -48,6 +48,8 @@ static void Main() // Requerimiento 9 Alcance de variables controlado (solo loca
         return false;
     }
 
+
+// Requerimientos 4 y 12: Función sin retorno que muestra las opciones válidas.
 static void MostrarMenu()
     {
         Console.WriteLine("\nTipos de consulta válidos:");
@@ -62,7 +64,7 @@ static string CalcularPrioridad(string tipoConsulta)
         
         return "Baja"; 
     }
-
+// Requerimientos 6 y 12: Valida que el texto no esté vacío y cumpla la longitud.
     static bool ValidarTexto(string texto, int longitudMinima)
     {
         if (string.IsNullOrWhiteSpace(texto) || texto.Length < longitudMinima)
@@ -84,14 +86,7 @@ static string CalcularPrioridad(string tipoConsulta)
         Console.WriteLine($"Prioridad Asignada: {prio}");
         Console.WriteLine("============================================");
     }
-   /*
-   REQUERIMIENTO 11 - REGISTRO DE PRUEBAS EJECUTADAS EN CONSOLA:
-   1. Datos válidos: Código "U123456", Nombre "Justin", Tipo "pagos" -> Ejecución OK.
-   2. Código vacío/inválido: Se ingresó "" -> El programa detectó el error y lo volvió a pedir OK.
-   3. Tipo incorrecto: Se ingresó "deportes" -> Mostró error de validación OK.
-   4. Prioridad alta: Se probó con "matrícula" -> El sistema le asignó prioridad Alta OK.
-   5. Prioridad baja: Se probó con "constancia" -> El sistema le asignó prioridad Baja OK.
-   */
+
 }
 
 
